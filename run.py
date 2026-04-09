7521813b2ca943a5811e96501c2b2ad4ff2ee1e6
parmartanishq <parmartanishq07@gmail.com>
2026-04-09 22:25:59 +0530

    fix port bindin

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
+import os
 import uvicorn
 
 def main():
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     main()
